1a47dc6a7dc315c9643418fa283d22e4729c95f7
Kapitolos <66430560+Kapitolos@users.noreply.github.com>
2020-07-08 13:45:36 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -10,12 +10,12 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data["email"]
-        subject = data["subject"]
-        message = data["message"]
-        file =database.write(f'\n{email},{subject},{message}')
+#def write_to_file(data):
+#    with open('database.txt', mode='a') as database:
+#        email = data["email"]
+#        subject = data["subject"]
+#        message = data["message"]
+#        file =database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
     with open('database.csv', newline='',mode='a') as database2:
@@ -30,7 +30,7 @@ def submit_form():
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
-            write_to_file(data)
+            #write_to_file(data)
             write_to_csv(data)
             return redirect('/thanks.html')
         except:
